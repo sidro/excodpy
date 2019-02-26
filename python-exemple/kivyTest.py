71d40aef83eaf140a7cfff38bc3895181bf703dd
sidro <slacky2005@gmail.com>
2019-02-26 21:10:48 +0200

    new

--- python-exemple/kivyTest.py
+++ python-exemple/kivyTest.py
@@ -6,7 +6,7 @@ from kivy.uix.button import Button
 
 class TestApp(App):
     def build(self):
-        return Button(text='Hello World')
+        return Button(text='Hello World!')
 
 
 if __name__ == "__main__":
