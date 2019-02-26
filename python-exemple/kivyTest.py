eed4a29b190499dd9643793dcd048d11192c1b58
sidro <slacky2005@gmail.com>
2019-02-26 20:40:09 +0200

    flake kivyTest

--- python-exemple/kivyTest.py
+++ python-exemple/kivyTest.py
@@ -3,9 +3,11 @@
 from kivy.app import App
 from kivy.uix.button import Button
 
+
 class TestApp(App):
     def build(self):
         return Button(text='Hello World')
-    
-if __name__ == "__main__" :
+
+
+if __name__ == "__main__":
     TestApp().run()
