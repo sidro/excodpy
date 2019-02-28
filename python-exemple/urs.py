35741eacd0d09a3c0632de450c3f5630b20527cb
sidro <slacky2005@gmail.com>
2019-02-28 20:38:15 +0200

    New file added

---
+++ python-exemple/urs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Mono:
+
+    def __init__(self, nume="Vulpea", culoare="portocalie", inaltime=65):
+        self.nume = nume
+        self.culoare = culoare
+        self.inaltime = inaltime
+
+    def prezinta(self):
+        print(f'El/Ea este {self.nume}, are culoarea {self.culoare} si inaltimea de {self.inaltime} cm.')
+
+        
+m = Mono("Urs", "bruna", 200)
+m.prezinta()
+#m2 = Mono()        # Prima varianta de a construi
+#m2.prezinta()      # un obiect Mono si a apela functia prezinta
+Mono().prezinta()   # A doua metoda 
