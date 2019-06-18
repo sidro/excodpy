833ceb1b8272d26776225e4bb333e0b5244e8dee
sidro <slacky2005@gmail.com>
2019-06-18 21:09:42 +0300

    cosmetizare

--- python-exemple/dict_unpack.py
+++ python-exemple/dict_unpack.py
@@ -6,15 +6,16 @@ def calculate(**kwargs):
         "divide": kwargs.get("first", 0) / kwargs.get("second", 0)
     }
     is_float = kwargs.get("make_float", False)
-    result = operations[kwargs.get("operation",'')]
+    result = operations[kwargs.get("operation", '')]
     if is_float:
-        final = "{} {}".format(kwargs.get("message","The result is "), float(result))
+        final = "{} {}".format(kwargs.get("message",
+                                          "The result is "), float(result))
     else:
-        final = "{} {}".format(kwargs.get("message","The result is "), int(result))
+        final = "{} {}".format(kwargs.get("message",
+                                          "The result is "), int(result))
     return final
 
 
-
-
-print(calculate(make_float=False, operation='multiply', message='You just added', first=2, second=4))
+print(calculate(make_float=False, operation='multiply',
+                message='You just added', first=2, second=4))
 print(calculate(make_float=True, operation='divide', first=3.5, second=5))
