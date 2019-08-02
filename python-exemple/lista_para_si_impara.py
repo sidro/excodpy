e459396d8a93f81d4b0db00fa5b801c3c4770023
sidro <slacky2005@gmail.com>
2019-08-02 15:17:39 +0300

    reimplementare functie part

--- python-exemple/lista_para_si_impara.py
+++ python-exemple/lista_para_si_impara.py
@@ -12,5 +12,10 @@ def partition(list_, funct):
             [li for li in list_ if not funct(li)]]
 
 
+def part(list_, fnc):
+    return [[list_.pop(list_.index(e)) for e in list_ if isEven(e)], list_]
+
+
 print(isEven(3))
 print(partition([1, 2, 3, 4], isEven))
+print(part([1, 2, 3, 4, 6, 7, 8], isEven))
